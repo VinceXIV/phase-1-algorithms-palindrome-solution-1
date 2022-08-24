65ed7dbf271359c823b7b7af6d90bbdbbd555682
Vincent <vincent.makokha@student.moringaschool.com>
2022-08-24 11:07:20 +0300

    Completed Assinment

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return reverseWord(word) === word
+}
+
+function reverseWord(word){
+  return word.split("").reverse().join("")
 }
 
 /* 
   Add your pseudocode here
+
+  1. Take the word and reverse it
+  2. Check the reversed word to confirm it is similar with the original word
+  3. If similar, return true (is palindrome). If not, return false
+
 */
 
 /*
@@ -16,10 +26,22 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
   console.log("");
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("great"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("awesome"));
 }
 
 module.exports = isPalindrome;
